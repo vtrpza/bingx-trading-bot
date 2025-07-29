3714963e414e25d643a3827cf7ca6809870d5d1c
Vitor <vhnpouza@gmail.com>
2025-07-29 00:25:37 -0300

    1

--- client/src/components/BotControls.tsx
+++ client/src/components/BotControls.tsx
@@ -179,22 +179,10 @@ const InputField = ({
   currency?: string
 }) => {
   const rules = VALIDATION_RULES[field as keyof typeof VALIDATION_RULES]
-  const tooltip = t(`trading.config.tooltips.${field}`)
-  
   return (
     <div>
       <label className="label flex items-center">
         <span>{label}</span>
-        {tooltip && (
-          <div className="group relative ml-2">
-            <svg className="w-4 h-4 text-gray-400 hover:text-gray-600 cursor-help" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clipRule="evenodd" />
-            </svg>
-            <div className="hidden group-hover:block absolute z-10 left-0 bottom-full mb-2 w-64 p-2 text-xs text-white bg-gray-800 rounded shadow-lg">
-              {tooltip}
-            </div>
-          </div>
-        )}
         {currency && <span className="ml-auto text-sm text-gray-500">({currency})</span>}
       </label>
       <input
